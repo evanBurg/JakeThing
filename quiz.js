a834cd773a6e536c7180d1e5f81002120eeb5642
Evan <evan@DESKTOP-VFQ0KLU>
2018-03-12 21:02:13 -0400

    jake do

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var questionsJson = {
-		"questionOne": {
+		1: {
 			title: "This is my first question",
 			body: "This is my first question",
 			answers: {
@@ -11,7 +11,7 @@ $(document).ready(function(){
 			},
 			correct: "A"
 		},
-		"questionTwo": {
+		2: {
             title: "This is my second question",
             body: "This is my second question",
             answers: {
@@ -20,20 +20,61 @@ $(document).ready(function(){
                 'C': 'Answer C',
                 'D': 'Answer D'
             },
-            correct: "A"
+            correct: "B"
+        },
+        3: {
+            title: "This is my third question",
+            body: "This is my third question",
+            answers: {
+                'A': 'Answer A',
+                'B': 'Answer B',
+                'C': 'Answer C',
+                'D': 'Answer D'
+            },
+            correct: "C"
         },
 		};
-	
-	$.each(questionsJson, function(index, element){
-		$('#questionsContainer').append('<div class="question-canvas">' +
-			'<h3>' + element.title + '</h3>' +
-			'<p>' + element.body + '</p>');
-			$.each(element.answers, function(indexA, elementA){
-				$('#questionsContainer').append(
-				'<span>'+ elementA +'</span>' +
-				'<input type="radio" name="radio-'+index+'">');
+
+    var htmlString = '';
+    var currentQuestion = null;
+	$.each(questionsJson, function(questionIndex, question){
+
+        htmlString += ('<div class="question-canvas" id="question-'+ questionIndex +'" data-correct="'+ question.correct +'" style="display: none">' +
+			'<h3>' + question.title + '</h3>' +
+			'<p>' + question.body + '</p>');
+			$.each(question.answers, function(answerIndex, answer){
+                htmlString += (
+				'<span>'+ answer +'</span>' +
+				'<input type="radio" class="question-select" name="radio-'+questionIndex+'" data-answer-id="'+answerIndex+'" data-question-id="'+ questionIndex +'">');
 			});
-	});	
-	$('#questionsContainer').append('</div');
+        htmlString += ('</div>');
+	});
+	$('#questionsContainer').html(htmlString);
+
+
+    $("#question-1").fadeIn();
+    currentQuestion = "1";
+
+		$('.question-select').on('click', function(event){
+		if($(this).data('answer-id') === $('#question-'+$(event.target).data('question-id')).data('correct')){
+			alert('Good job jake');
+		}else{
+			alert('Bad');
+		}
+	});
+
+	$('#navbar').click(function(event){
+        $("#question-"+currentQuestion).hide();
+		var question = $(event.target).closest('button').attr('data-page');
+        $("#question-"+question).fadeIn();
+		$('#last').attr('data-page', currentQuestion);
+		$('#next').attr('data-page', parseInt(question)+1);
+		if(currentQuestion < 1){
+            $('#last').hide();
+		}else{
+            $('#last').fadeIn();
+		}
+		currentQuestion = question;
+	});
 	
 });
